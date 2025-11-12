8a7aa368cec43e5d4c99480ce05bf3a9e864544e
shivamgulia00-sketch <shivamgulia00@gmail.com>
2025-11-12 12:34:07 +0530

    Add files via upload

---
+++ new file.py
@@ -0,0 +1,38 @@
+print("welcome")
+def run():
+  
+      print("press 1 for continue")
+      print("press 2 for Exit")
+
+a=int(input("Continue or exit"))
+if a==1:
+  print("Continue")
+elif a==2:
+  print("Thankyou for visiting")
+
+def book():
+    print("press 1 for book1")
+    print("press 2 for book2")
+    print("press 3 for book3")
+    print("press 4 for book4")
+
+    
+b=int(input("Enter book number to continue"))
+if b==1:
+   print("psychology of money:--The Psychology of Money is the study of how our behaviors", "emotions, and mental biases influence our financial decisions, rather than our intelligence")
+
+
+   run()
+elif b==2:
+    print("book2:--book description")
+
+    run()
+elif b==3:
+     print("book3:--book description")
+
+     run()
+elif b==4:
+     print("book4:--book descroption")
+
+     run()
+
